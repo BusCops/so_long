a27d9868e81d5367054dde9649c4ec5578823da5
Abdellah Benzahour <abenzaho@e1r11p9.1337.ma>
2025-02-19 15:01:58 +0100

    ok

--- so_long_utils/so_long_util5.c
+++ so_long_utils/so_long_util5.c
@@ -6,7 +6,7 @@
 /*   By: abenzaho <abenzaho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 13:58:38 by abenzaho          #+#    #+#             */
-/*   Updated: 2025/02/19 13:20:30 by abenzaho         ###   ########.fr       */
+/*   Updated: 2025/02/19 14:58:22 by abenzaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,23 +81,32 @@ void	save_coin_image(t_game *game)
 			&game->coin.img.endian);
 }
 
+int	get_t(int trgb)
+{
+	return ((trgb >> 24) & 0xFF);
+}
+
 void	draw_coin_image(t_game *game, int k, int pos)
 {
 	t_index			in;
 	unsigned int	color;
 	char			*pixel_bg;
 	char			*pixel_cn;
-	
+	int				j;
+
+	j = 0;
+	if (k > 2)
+		j = j + 150;
 	in.i = 0;
 	while (in.i < 150)
 	{
 		in.j = 0;
 		while (in.j < 150)
-		{
+		{		
 			pixel_bg = get_pixel_in_bg(game->bg.img, game->coin.cpos[pos]->x, game->coin.cpos[pos]->y, in);
 			pixel_cn = get_pixel_from_image(game->coin.anim, in.j, in.i);
-			color = get_color(game->coin.img, (k * 150) + in.j, in.i);
-            if (color == 4278190080)
+			color = get_color(game->coin.img, (k * 150) + in.j, in.i + j);
+            if (get_t(color))
             {
                 color = *(unsigned int *)pixel_bg;
             }
@@ -126,6 +135,7 @@ int	fill_coin(t_game *game)
 			}
 			i++;
 		}
+		//mlx_do_sync(game->mlx);
 		usleep(100000);
 		j++;
 	}
